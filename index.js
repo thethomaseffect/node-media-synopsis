16bcbf532ad9aeed3521a96c6c027403732e0971
Thomas Geraghty <thethomaseffect@gmail.com>
2014-07-17 21:47:46 +0100

    Reindented source

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ exports.synopsis = function(mediaTitle, mediaType, callback) {
     if (mediaType === null || mediaType === undefined) {
         mediaType = '';
     }
-    google(mediaTitle + ' '+ mediaType +' site:wikipedia.org', function(err, next, links){
-        if (err) {
-            callback(err);
-            return;
-        }
-        if (!links[0]) {
-            callback("No Wikipedia Results found for that mediaTitle and mediaType");
-            return;
-        }
-        // Sometimes this will be 'List of x Episodes' for TV shows. Example: 're hamatora'
-        var title = getMediaTitle(links[0].title);
-        request(links[0].link, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            data = extractor(body);
-            var releventParagraphs = getParagraphsUntilMediaMention(data.text, mediaType);
-            callback(undefined,
-            {
-                title: title,
-                content: releventParagraphs.replace(/(\[.+\])/g,'') // Remove annotations
-            });
+    google(mediaTitle + ' ' + mediaType + ' site:wikipedia.org',
+        function(err, next, links){
+            if (err) {
+                callback(err);
+                return;
+            }
+            if (!links[0]) {
+                callback("No Wikipedia Results found for that mediaTitle and mediaType");
+                return;
+            }
+            // Sometimes this will be 'List of x Episodes' for TV shows. Example: 're hamatora'
+            var title = getMediaTitle(links[0].title);
+            request(links[0].link, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                data = extractor(body);
+                var releventParagraphs = getParagraphsUntilMediaMention(data.text, mediaType);
+                callback(undefined,
+                {
+                    title: title,
+                    content: releventParagraphs.replace(/(\[.+\])/g,'') // Remove annotations
+                });
         } else {
             callback(error);
         }
