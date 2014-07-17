d33705e9a290d5a72a7049addd52fc1212e38e2d
Thomas Geraghty <thethomaseffect@gmail.com>
2014-07-17 20:54:28 +0100

    Added initial source files and package.json

---
+++ wiki.js
@@ -0,0 +1,68 @@
+var extractor = require('unfluff');
+var request = require('request');
+var google = require('google');
+
+function getMediaTitle(fullLinkText) {
+    var title = fullLinkText.split('- Wikipedia')[0];
+    var listOfEpsRegex = /.*List of (.*) episodes/.exec(title);
+    if(listOfEpsRegex) {
+        return listOfEpsRegex[1];
+    }
+    return title;
+}
+
+function getParagraphsUntilMediaMention(text, mediaType) {
+    paragraphs = text.split('\n');
+    var re = new RegExp('\\b' + mediaType + '\\b', 'i');
+    var output = '';
+    var index = 0;
+    while(index <= paragraphs.length) {
+        if(re.test(paragraphs[index]) || mediaType === ''){
+            // If there's already stuff in there make a new line,
+            // otherwise just return the current paragraph
+            return output.length > 0 ?
+            output + '\n\n' + paragraphs[index] :
+            paragraphs[index];
+        }
+        output += paragraphs[index]; // Add whatever was found to output buffer
+        index += 1;
+    }
+    // Word anime wasn't found, return falsey value
+    return '';
+}
+
+function getWikipediaContent(mediaTitle, mediaType, callback) {
+    if (mediaType === null || mediaType === undefined) {
+        mediaType = '';
+    }
+    google(mediaTitle + ' '+ mediaType +' site:wikipedia.org', function(err, next, links){
+        if (err) {
+            callback(err);
+            return;
+        }
+        if (!links[0]) {
+            callback("No Wikipedia Results found for that mediaTitle and mediaType");
+            return;
+        }
+        // Sometimes this will be 'List of x Episodes' for TV shows. Example: 're hamatora'
+        console.log(getMediaTitle(links[0].title));
+        console.log();
+        request(links[0].link, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            data = extractor(body);
+            var releventParagraphs = getParagraphsUntilMediaMention(data.text, mediaType);
+            callback(undefined, releventParagraphs.replace(/(\[.+\])/g,'')); // Remove annotations
+        } else {
+            callback(error);
+        }
+    });
+    });
+}
+
+getWikipediaContent('gedo senki', 'anime', function(err, text) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(text);
+});
