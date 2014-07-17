1e992d66850cac1e4500716884c63195c791c218
Thomas Geraghty <thethomaseffect@gmail.com>
2014-07-17 21:33:41 +0100

    Move exported function to top of script in wiki.js

--- wiki.js
+++ wiki.js
@@ -2,35 +2,6 @@ var extractor = require('unfluff');
 var request = require('request');
 var google = require('google');
 
-function getMediaTitle(fullLinkText) {
-    var title = fullLinkText.split('- Wikipedia')[0];
-    var listOfEpsRegex = /.*List of (.*) episodes/.exec(title);
-    if(listOfEpsRegex) {
-        return listOfEpsRegex[1];
-    }
-    return title;
-}
-
-function getParagraphsUntilMediaMention(text, mediaType) {
-    paragraphs = text.split('\n');
-    var re = new RegExp('\\b' + mediaType + '\\b', 'i');
-    var output = '';
-    var index = 0;
-    while(index <= paragraphs.length) {
-        if(re.test(paragraphs[index]) || mediaType === ''){
-            // If there's already stuff in there make a new line,
-            // otherwise just return the current paragraph
-            return output.length > 0 ?
-            output + '\n\n' + paragraphs[index] :
-            paragraphs[index];
-        }
-        output += paragraphs[index]; // Add whatever was found to output buffer
-        index += 1;
-    }
-    // Word anime wasn't found, return falsey value
-    return '';
-}
-
 exports.synopsis = function(mediaTitle, mediaType, callback) {
     if (mediaType === null || mediaType === undefined) {
         mediaType = '';
@@ -61,3 +32,33 @@ exports.synopsis = function(mediaTitle, mediaType, callback) {
     });
     });
 };
+
+
+function getMediaTitle(fullLinkText) {
+    var title = fullLinkText.split('- Wikipedia')[0];
+    var listOfEpsRegex = /.*List of (.*) episodes/.exec(title);
+    if(listOfEpsRegex) {
+        return listOfEpsRegex[1];
+    }
+    return title;
+}
+
+function getParagraphsUntilMediaMention(text, mediaType) {
+    paragraphs = text.split('\n');
+    var re = new RegExp('\\b' + mediaType + '\\b', 'i');
+    var output = '';
+    var index = 0;
+    while(index <= paragraphs.length) {
+        if(re.test(paragraphs[index]) || mediaType === ''){
+            // If there's already stuff in there make a new line,
+            // otherwise just return the current paragraph
+            return output.length > 0 ?
+            output + '\n\n' + paragraphs[index] :
+            paragraphs[index];
+        }
+        output += paragraphs[index]; // Add whatever was found to output buffer
+        index += 1;
+    }
+    // Word anime wasn't found, return falsey value
+    return '';
+}
